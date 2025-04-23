5c86b172e41d313a147b7aa99ed004d4c2a86600
Hong-CHP <93changhong@gmail.com>
2025-04-23 10:31:14 +0200

    modification_memo_for_bonus_part

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -18,33 +18,35 @@ char    *cut_line(char *dest, char **rest)
         *rest = ft_strdup(&dest[end + 1]);
     else
         *rest = NULL;
-    free(dest);
     return (line);
 }
 
 char    *allo_init_buf(int fd, char **dest)
 {
     char    *buff;
-    size_t  bytes_read;
+    char    *temp;
+    int  bytes_read;
 
-    if (fd <= 0 || BUFFER_SIZE <= 0)
-        return (NULL);
     buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
     if (!buff)
         return (NULL);
     if (!*dest)
         *dest = ft_strdup("");
-    while (ft_strchr(*dest, '\n') == NULL)
+    bytes_read = 1;
+    while (bytes_read > 0 && ft_strchr(*dest, '\n') == NULL)
     {
         bytes_read = read(fd, buff, BUFFER_SIZE);
         if (bytes_read <= 0)
         {
             free(buff);
-            return (NULL);
+            return (*dest);
         }
         buff[bytes_read] = '\0';
+        temp = *dest;
         *dest = ft_strjoin(*dest, buff);
+        free(temp);
     }
+    free(buff);
     return (*dest);
 }
 
@@ -52,12 +54,36 @@ char    *get_next_line_bonus(int fd)
 {
     char    *line;
     char    *rest;
+    char    *temp;
     static char    *dest[1024];
 
-    dest[fd] = allo_init_buf(fd, &dest[fd]);
+    if (fd <= 0 || fd >= 1024 || BUFFER_SIZE <= 0)
+        return (NULL);
+    temp = allo_init_buf(fd, &dest[fd]);
+    if (!temp)
+        return (NULL);
+    dest[fd] = temp;
+    if (dest[fd][0] == '\0')
+    {
+        free(dest[fd]);
+        dest[fd] = NULL;
+        return (NULL);
+    }
     line = cut_line(dest[fd], &rest);
+    free(dest[fd]);
     dest[fd] = rest;
     return (line);
+}
+
+void    ft_print_line(int fd)
+{
+    char    *line;
+
+    while ((line = get_next_line_bonus(fd)) != NULL)
+    {
+        printf("%s\n", line);
+        free(line);
+    }
 }
 
 void ft_display_file(char *file1, char *file2, char *file3)
@@ -65,15 +91,17 @@ void ft_display_file(char *file1, char *file2, char *file3)
     int fd1;
     int fd2;
     int fd3;
+    char    *line;
 
     fd1 = open(file1, O_RDONLY);
     fd2 = open(file2, O_RDONLY);
     fd3 = open(file3, O_RDONLY);
-    printf("%s\n", get_next_line_bonus(fd1));
-    printf("%s\n", get_next_line_bonus(fd2));
-    printf("%s\n", get_next_line_bonus(fd3));
-    printf("%s\n", get_next_line_bonus(fd1));
-    printf("%s\n", get_next_line_bonus(fd2));
+    line = get_next_line_bonus(fd2);
+    printf("%s\n", line);
+    free(line);
+    // ft_print_line(fd1);
+    // ft_print_line(fd2);
+    // ft_print_line(fd3);
     close(fd1);
     close(fd2);
     close(fd3);
